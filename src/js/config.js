6d290b792a49f9f49c80d4e948ded467fcea0a8e
Adrian Stainforth <adrian.stainforth@gmail.com>
2020-04-13 14:43:34 +0100

    Simplify config

--- src/js/config.js
+++ src/js/config.js
@@ -4,32 +4,29 @@ import rock from '../images/rock.svg';
 import scissors from '../images/scissors.svg';
 import spock from '../images/spock.svg';
 /* 
-Configs are adaptable so new options can be added or new config created
+Configs are adaptable so new options can be added or new config created.  Config designed as simple API to simplify task of adding new options
 
 basic config is as so:
 
 title : {
   image: 'path to image',
-  win: {title: response} or [{title: response}, ...]
-  lose: title or [title, ...]
+  win: [{title: response}, ...]
+  lose: [title, ...]
 }
 
 */
 export const DEFAULT_CONFIG = {
   rock: {
     image: rock,
-    win: {scissors: 'blunts'},
-    lose: 'paper'
+    win: [{scissors: 'blunts'}],
   },
   paper: {
     image: paper,
-    win: {rock: 'covers'},
-    lose: 'scissors'
+    win: [{rock: 'covers'}],
   },
   scissors: {
     image: scissors,
-    win: {paper: 'cuts'},
-    lose: 'rock'
+    win: [{paper: 'cuts'}],
   },
 }
 
@@ -37,26 +34,21 @@ export const ADVANCED_CONFIG = {
   rock: {
     image: rock,
     win: [{scissors: 'blunts'}, {lizard: 'crushes'}],
-    lose: ['paper', 'spock']
   },
   paper: {
     image: paper,
     win: [{rock: 'covers'}, {spock: 'disproves'}],
-    lose: ['scissors' , 'lizard']
   },
   scissors: {
     image: scissors,
     win: [{paper: 'cuts'}, {lizard: 'decapitates'}],
-    lose: ['rock', 'spock']
   },
   spock: {
     image: spock,
     win: [{scissors: 'smashes'}, {rock: 'vaporises'}],
-    lose: ['paper', 'lizard']
   },
   lizard: {
     image: lizard,
     win: [{paper: 'eats'}, {spock: 'poisons'}],
-    lose: ['rock', 'scissors']
   },
 }
