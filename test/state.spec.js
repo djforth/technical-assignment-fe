747da6bba24f5fe72b42ca22cc4e8ea7ed96a444
Adrian Stainforth <adrian.stainforth@gmail.com>
2020-04-13 19:00:49 +0100

    Rename test to state

--- test/state.spec.js
+++ test/state.spec.js
@@ -1,51 +1,51 @@
-import Options from 'js/create_options';
+import State from 'js/state';
 import {DEFAULT_CONFIG, ADVANCED_CONFIG} from 'js/config';
 
 import { defaultOptions, advancedOptions } from './__helpers__/options.helper';
 
 // Abstract tests to keep them DRY
-const checkOptionsOpt = function(title, config, expected) {
+const checkState = function(title, config, expected) {
   describe(`Should create ${title}`, () => {
-    let options;
+    let state;
     beforeAll(()=>{
-      options = Options(config);
+      state = State(config);
     });
 
     test('should have correct selections', () => {
-      expect(options.selections).toEqual(expect.arrayContaining(expected.selections));
+      expect(state.selections).toEqual(expect.arrayContaining(expected.selections));
     });
 
     test('should have correct images', () => {
-      expect(options.selections).toEqual(expect.arrayContaining(expected.selections));
+      expect(state.selections).toEqual(expect.arrayContaining(expected.selections));
     });
   });
 }
 
 describe('Should create valid options', () => {
   // Checks default options
-  checkOptionsOpt('default options', DEFAULT_CONFIG, defaultOptions);
+  checkState('default state', DEFAULT_CONFIG, defaultOptions);
   // Checks advanced options
-  checkOptionsOpt('advanced options', ADVANCED_CONFIG, advancedOptions);
+  checkState('advanced state', ADVANCED_CONFIG, advancedOptions);
 
   describe('If config is incorrect', () => {
     test('if no config', () => {
-      const failure = ()=>Options('foo');
+      const failure = ()=>State('foo');
       expect(failure).toThrowError('Config not an object, please check config');
     });
 
     test('if config less then 2', () => {
-      const failure = ()=>Options({rock: {image:'', lose:'', win:''}});
+      const failure = ()=>State({rock: {image:'', win:''}});
       expect(failure).toThrowError('Config must have at least 2 options, please check config');
     });
 
     test('if config incorrect with item not correct keys', () => {
-      const failure = ()=>Options({
-        rock: {image:'', lose:'', win:''}, 
+      const failure = ()=>State({
+        rock: {image:'', win:''}, 
         paper: {win:'' },
-        scissors: {image:'', lose:'', win:''}
+        scissors: {image:'', win:''}
       });
       // expect(['win'].includes('win')).toBeTruthy();
-      expect(failure).toThrowError("Config must have 'image', 'win', 'lose' for each item, please check config");
+      expect(failure).toThrowError("Config must have 'image', 'win' for each item, please check config");
     });
   });
 });
